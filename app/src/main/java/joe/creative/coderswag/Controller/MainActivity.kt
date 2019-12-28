b1f3a25c877a83060e9327f953b821b14027b2e1
Joseph Martin <creative.joe007@gmail.com>
2019-12-28 23:45:28 +0100

    Added on item click listener

--- app/src/main/java/joe/creative/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/joe/creative/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package joe.creative.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import joe.creative.coderswag.Adapters.CategoryAdapter
 import joe.creative.coderswag.Model.Category
 import joe.creative.coderswag.R
@@ -20,5 +21,11 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener { parent, view, position, id ->
+            val category = DataService.categories[position]
+            Toast.makeText(this, "Clicked category name is ${category.title}", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
